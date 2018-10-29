96adc7c62c05a66b41d791ae71a6e8b26d0f3819
SoojiNoh <thensj6873@gmail.com>
2018-10-29 15:34:44 +0900

    .

--- routes/workspaces.js
+++ routes/workspaces.js
@@ -6,5 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('workspaces/index', {title : 'Chaining'})
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('workspaces/new', {title : 'Chaining'})
+});
+
+
 
 module.exports = router;
